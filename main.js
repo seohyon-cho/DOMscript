7fdc04bf1a0a5d8a79f3640f9904e3f7af461c31
seohyon-cho <flanique@naver.com>
2023-11-10 12:14:33 +0900

    DOM의 요소 선택하는 명령어들 (querySelector, parentElement, children 등..)

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/*
+  - document.querySelector('section'); 태그명으로 단수 요소
+  - section.querySelectorAll('article'); 태그명으로 복수 개 요소
+  - section.children; 모든 직계자식
+  - btn.parentElement; 직계부모
+  - btn.closest('section') 조상 요소
+  - btn.previousElementSiblings; 특정 요소의 이전 형제요소
+  - btn.nextElementSiblings; 특정 요소의 다음 형제요소
+  - btn.parentElement.children; 모든 형제요소 
+      (모든 형제요소를 찾는 공식적인 규칙은 없어서, parentElement를 통해 부모로 한 단계 상위로 올라갔다가 children으로 그 부모의 모든 자식을 구하는 것.)
+ */
+
 const section = document.querySelector('section');
 const articles = section.querySelectorAll('article');
 const btn = section.querySelector('.btn');
